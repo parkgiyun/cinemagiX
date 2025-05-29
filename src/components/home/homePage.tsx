f8ef77224b1e3099ed7a5278588993baaf4cacc2
kiyoon <99074153@naver.com>
2025-05-29 22:41:58 +0900

    소셜 로그인 수정 시도

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -3,14 +3,13 @@
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { HomeContent } from "./homeUI";
-import { Suspense } from "react";
 
 function SocialAutoLogin() {
   const router = useRouter();
   const params = useSearchParams();
-  const isSocial = params.get("social") === "1";
 
   useEffect(() => {
+    const isSocial = params.get("social") === "1";
     if (isSocial) {
       fetch("https://hs-cinemagix.duckdns.org/api/v1/user/me", {
         method: "POST",
@@ -25,7 +24,7 @@ function SocialAutoLogin() {
           router.push("/login");
         });
     }
-  }, [isSocial, router]);
+  }, [params, router]);
 
   return null;
 }
@@ -33,9 +32,7 @@ function SocialAutoLogin() {
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-      <Suspense fallback={null}>
-        <SocialAutoLogin />
-      </Suspense>
+      <SocialAutoLogin />
       <HomeContent />
     </div>
   );
